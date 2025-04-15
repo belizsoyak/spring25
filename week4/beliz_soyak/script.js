7107c6fcf3298575e5c2792857c15162f239e48e
belizsoyak <beliz.soyak@gmail.com>
2025-04-15 14:38:47 -0700

    week 4 changes

--- week4/beliz_soyak/script.js
+++ week4/beliz_soyak/script.js
@@ -1,4 +1,28 @@
+let clickTimes = [];
+
 function addOneToCounter() {
-    document.getElementById("counter").innerText = 
-    parseInt(document.getElementById("counter").innerText) + 1
+    const counter = document.getElementById("counter");
+    const clickSound = document.getElementById("clickSound");
+    const cpsDisplay = document.getElementById("cps");
+
+    counter.innerText = parseInt(counter.innerText) + 1;
+    clickSound.play();
+
+    clickTimes.push(Date.now());
+
+    setInterval(() => {
+        const now = Date.now();
+        // Only keep clicks from the last 2 seconds
+        clickTimes = clickTimes.filter(time => now - time <= 2000);
+    
+        const cps = (clickTimes.length / 2).toFixed(2);
+        document.getElementById("cps").innerText = `Speed: ${cps} clicks/sec`;
+    }, 200);
+
+    
 }
+
+function resetCounter() {
+    document.getElementById("counter").innerText = 0;
+}
+
