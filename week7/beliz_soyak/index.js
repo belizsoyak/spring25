7b080eb73069e58f4050dcc372530d66841f74f7
belizsoyak <beliz.soyak@gmail.com>
2025-04-15 21:18:37 -0700

    week 7 added

--- week7/beliz_soyak/index.js
+++ week7/beliz_soyak/index.js
@@ -5,12 +5,31 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://sam:helloworld@cluster0.ytnrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  nick: String,
+  msg: String,
+  timestamp: { type: Date, default: Date.now }
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  Message.find().sort({ timestamp: 1 }).limit(20).then((msgs) => {
+    msgs.forEach((m) => {
+      socket.emit('chat message', { nick: m.nick, msg: m.msg });
+    });
+  });
 
   socket.nickname = 'Anonymous';
 
@@ -23,8 +42,18 @@ io.on('connection', (socket) => {
   });
 
   socket.on('chat message', ({ msg, nick }) => {
+
+    const message = new Message({ msg, nick }); 
+    
+    message.save().then(() => {
+      console.log('Message saved to MongoDB:', msg, 'from', nick);
+    }).catch(err => {
+      console.error('❌ Failed to save message:', err);
+    });
+
     io.emit('chat message', { msg, nick });
   });
+  
 
   socket.on('disconnect', () => {
     socket.broadcast.emit('chat message', {
@@ -32,6 +61,8 @@ io.on('connection', (socket) => {
       msg: `🔴 ${socket.nickname} has disconnected`
     });
   });
+
+
   socket.on('typing', function(nick) {
     socket.broadcast.emit('typing', nick);
   });
